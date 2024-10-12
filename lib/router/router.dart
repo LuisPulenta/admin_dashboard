48b7ee12b6362f4b4e970247ddbb319283ac0ea2
Luis Núñez <luisalbertonu@gmail.com>
2024-10-12 19:10:12 -0300

    Initial commit

---
+++ lib/router/router.dart
@@ -0,0 +1,30 @@
+import 'package:fluro/fluro.dart';
+import 'package:admin_dashboard/router/admin_handlers.dart';
+
+class Flurorouter {
+  static final FluroRouter router = FluroRouter();
+
+  static String rootRoute = '/';
+
+  //Auth Router
+  static String loginRoute = '/auth/login';
+  static String registerRoute = '/auth/register';
+
+  //Dashboard Router
+  static String dashboardRoute = '/dashboard';
+
+  static void configureRoutes() {
+    router.define(
+      rootRoute,
+      handler: AdminHandlers.login,
+    );
+    router.define(
+      loginRoute,
+      handler: AdminHandlers.login,
+    );
+    //  router.define(
+    //   registerRoute,
+    //   handler: loginHandler,
+    // );
+  }
+}
